59ad56e5d8062cdf57953a64f3358ba9901fa5cf
Jaydatt <jaydattsonijs@gmail.com>
2024-04-18 11:04:41 +0530

    Added Github Link in Footer

--- MemoryCraft/src/Components/Footer.jsx
+++ MemoryCraft/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div className='text-2xl font-bold flex justify-center items-center h-16 w-full bg-gray-700'>
-      Made By&nbsp;<Link to='https://www.linkedin.com/in/jaydattdholakiya/' className=' hover:text-logo-color'> Jaydatt</Link>
+      Made By&nbsp;<Link to='https://github.com/Jaydatt-D' className=' hover:text-logo-color'> Jaydatt</Link>
     </div>
   )
 }
